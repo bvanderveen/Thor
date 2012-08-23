247e41e05a5fcf8e6c29466613a31533125a8092
Benjamin van der Veen <b@bvanderveen.com>
2012-08-23 11:10:00 -0700

    scrollable targetview

--- Thor/Classes/TargetView.h
+++ Thor/Classes/TargetView.h
@@ -2,7 +2,9 @@
 
 @interface TargetView : NSView
 
+@property (nonatomic, strong) IBOutlet NSScrollView *scrollView;
+@property (nonatomic, strong) IBOutlet NSBox *settingsBox, *deploymentsBox;
 @property (nonatomic, strong) IBOutlet GridView *deploymentsGrid;
-@property (nonatomic, strong) NSButton *editButton;
+@property (nonatomic, strong) IBOutlet NSView *settingsView;
 
 @end
--- Thor/Classes/TargetView.m
+++ Thor/Classes/TargetView.m
@@ -1,8 +1,26 @@
 #import "TargetView.h"
-#import "CollectionView.h"
+#import "BoxGroupView.h"
+
+@interface TargetSettingsView : NSView
+
+@end
+
+@implementation TargetSettingsView
+
+- (NSSize)intrinsicContentSize {
+    return NSMakeSize(NSViewNoInstrinsicMetric, 135);
+}
+
+@end
 
 @implementation TargetView
 
-@synthesize deploymentsGrid, editButton;
+@synthesize scrollView, deploymentsGrid, deploymentsBox, settingsBox, settingsView;
+
+- (void)layout {
+    [BoxGroupView layoutInBounds:self.bounds scrollView:scrollView box1:settingsBox boxContent1:settingsView box2:deploymentsBox boxContent2:deploymentsGrid];
+    
+    [super layout];
+}
 
 @end
