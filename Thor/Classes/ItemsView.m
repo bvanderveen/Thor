1fa7f3684c338597088b57a318ccd422cb83eafb
Benjamin van der Veen <b@bvanderveen.com>
2012-09-04 20:19:51 -0700

    layout bar in items view. ugly but needed for now

--- Thor/Classes/ItemsView.m
+++ Thor/Classes/ItemsView.m
@@ -11,8 +11,9 @@
 
 - (void)layout {
     // TODO factor bar out of items view
-    bar.frame = NSZeroRect;
-    collectionScrollView.frame = self.bounds;
+    NSSize barSize = bar.intrinsicContentSize;
+    bar.frame = NSMakeRect(0, 0, self.bounds.size.width, barSize.height);
+    collectionScrollView.frame = NSMakeRect(0, barSize.height, self.bounds.size.width, self.bounds.size.height - barSize.height);
     [super layout];
 }
 
