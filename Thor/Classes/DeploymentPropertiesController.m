f47df8a2748f3cbc0d08c5fade16a457fd52b3e5
Benjamin van der Veen <b@bvanderveen.com>
2012-09-25 17:27:00 -0700

    ensure an app does not exist before creating it

--- Thor/Classes/DeploymentPropertiesController.m
+++ Thor/Classes/DeploymentPropertiesController.m
@@ -2,6 +2,24 @@
 #import "NSObject+AssociateDisposable.h"
 #import "RACSubscribable+ShowLoadingView.h"
 
+@interface RACSubscribable (ContinueAfter)
+
+- (RACSubscribable *)continueWith:(RACSubscribable *)subscribable;
+
+@end
+
+@implementation RACSubscribable (ContinueAfter)
+
+- (RACSubscribable *)continueWith:(RACSubscribable *)subscribable {
+    return [[self select:^id(id x) {
+        return subscribable;
+    }] selectMany:^id<RACSubscribable>(id x) {
+        return x;
+    }];
+}
+
+@end
+
 @interface DeploymentPropertiesController ()
 
 @property (nonatomic, strong) NSArray *apps; // of FoundryApp
@@ -18,16 +36,24 @@
     return self;
 }
 
-- (void)awakeFromNib {
-    FoundryService *service = [[FoundryService alloc] initWithEndpoint:[FoundryEndpoint endpointWithTarget:deployment.target]];
-    
-    RACSubscribable *call = [[service getApps] showLoadingViewInView:self.deploymentPropertiesView.contentView];
-    
-    self.associatedDisposable = [call subscribeNext:^(id x) {
-        self.apps = x;
-    } error:^(NSError *error) {
-        [NSApp presentError:error];
-    }] ;
+#define ThorDeploymentPropertiesControllerErrorDomain @"ThorDeploymentPropertiesControllerErrorDomain"
+#define ThorAppAlreadyExistsErrorCode 0xabcdbeef
+
+- (RACSubscribable *)ensureService:(FoundryService *)service doesNotHaveAppWithName:(NSString *)name {
+    return [RACSubscribable createSubscribable:^RACDisposable *(id<RACSubscriber> subscriber) {
+        return [[service getAppWithName:name]
+                subscribeNext:^ (id i) {
+                    [subscriber sendError:[NSError errorWithDomain:ThorDeploymentPropertiesControllerErrorDomain code:ThorAppAlreadyExistsErrorCode userInfo:@{
+                            NSLocalizedDescriptionKey : [NSString stringWithFormat:@"An app named %@ already exists on the host.", name]
+                    }]];
+                }
+                error:^ (NSError *error) {
+                    [subscriber sendNext:[NSNull null]];
+                    [subscriber sendCompleted];
+                }
+                completed:^ {
+                }];
+    }];
 }
 
 - (void)buttonClicked:(NSButton *)button {
@@ -53,7 +79,11 @@
             // TODO display spinner while waiting.
             deploymentPropertiesView.confirmButton.enabled = NO;
             
-            self.associatedDisposable = [[service createApp:app] subscribeError:^(NSError *error) {
+            RACSubscribable *subscribable = [[self ensureService:service doesNotHaveAppWithName:deployment.appName] continueWith:[service createApp:app]];
+            
+            self.associatedDisposable = [subscribable subscribeNext: ^ (id n) {
+                NSLog(@"%@", n);
+            } error:^(NSError *error) {
                 [NSApp presentError:error];
                 deploymentPropertiesView.confirmButton.enabled = YES;
             } completed:^{
