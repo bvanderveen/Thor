1df055203db21d2656d9f9d30a377241801c39fa
Benjamin van der Veen <b@bvanderveen.com>
2012-11-23 16:12:07 -0800

    [#80] disable modal window prevention of application termination. closes #80

--- Thor/Classes/SheetWindow.m
+++ Thor/Classes/SheetWindow.m
@@ -9,6 +9,7 @@
 + (SheetWindow *)sheetWindowWithView:(NSView *)view {
     SheetWindow *window = [[SheetWindow alloc] initWithContentRect:(NSRect){ .origin = NSZeroPoint, .size = view.intrinsicContentSize } styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
     
+    window.preventsApplicationTerminationWhenModal = NO;
     window.contentView = view;
     return window;
 }
